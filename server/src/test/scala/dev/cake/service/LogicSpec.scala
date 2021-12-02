50ea96847c837e710449831fb133edc30a0393b3
Reid Mewborne <rmewborne@gmail.com>
2021-12-02 17:03:24 -0500

    Sbt GitHub actions (#5)
    * adding a config for sbt
    
    * adding a config for sbt
    
    * custom jvm
    
    * add test
    
    * example of by hand file
    
    * test don't work
    
    * report test results
    
    * more reporting on test
    
    * more reporting on test
    
    * path probs
    
    * path probs.
    
    * path probs..
    
    * path probs...
    
    * path probs....
    
    * path probs.....
    
    * path probs......
    
    * test reporting should work now
    
    * don't use sbt-github-actions yet

---
+++ server/src/test/scala/dev/cake/service/LogicSpec.scala
@@ -0,0 +1,22 @@
+package dev.cake.service
+
+import dev.cake.fixtures.Fixtures.testName
+import org.scalatest.concurrent.ScalaFutures.convertScalaFuture
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.ExecutionContext
+
+class LogicSpec extends AnyFunSpec with Matchers {
+  implicit val ec = ExecutionContext.global
+  describe("service logic") {
+    val service =  new Logic()
+    describe("greeting") {
+      it("should return a friendly greeting given a name") {
+        val expected = s"hello, $testName!"
+        val greeting = service.greeting(testName).futureValue
+        greeting shouldEqual expected
+      }
+    }
+  }
+}
